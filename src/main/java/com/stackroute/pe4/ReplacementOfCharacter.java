0c968f9f9d538b9e2efc2ed0b8ea1c10bb2131bc
sushree mishra <sushree022@gmail.com>
2019-07-22 12:33:51 +0530

    test case for replacement of charcter added

---
+++ src/main/java/com/stackroute/pe4/ReplacementOfCharacter.java
@@ -0,0 +1,4 @@
+package com.stackroute.pe4;
+
+public class ReplacementOfCharacter {
+}
---
+++ src/test/java/com/stackroute/pe4/ReplacementOfCharacterTest.java
@@ -0,0 +1,42 @@
+package com.stackroute.pe4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ReplacementOfCharacterTest {
+    ReplacementOfCharacter replacementOfCharacter;
+
+    /* This methods runs, before running any one of the test case .
+            It is used to initialize the required variables
+     */
+    @Before
+    public void setup()
+    {
+//        arrange
+        replacementOfCharacter= new ReplacementOfCharacter();
+    }
+
+    /* This method runs, after running all the test cases
+              It is used to clear the initialized variables
+       */
+    @After
+    public void teardown()
+    {
+        replacementOfCharacter= null;
+    }
+
+    @Test
+    public void givenInputStringShouldReturnStringWithReplacedCharacters()
+    {
+//        act
+        String input= "daily dry";
+        String actualResult= replacementOfCharacter.replaceCharacter(input);
+//        assert
+        String expectedResult= "faity fry";
+        assertEquals(expectedResult,actualResult);
+    }
+
+}
